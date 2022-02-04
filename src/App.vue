dbe5d2fe1a30a610b75f823bb9d1bcb032159f04
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-02-03 21:33:12 -0500

    Clase 06: La función watch
    - Uso de "watch" para que se ejecute una acción cuando cambie una propiedad reactiva del modelo
    - Añade documentación
    - Refactoriza App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,30 @@ import tasksList from "./api/tasks.js";
 import AppTaskList from "./components/AppTaskList.vue";
 import AppTaskSearch from "./components/AppTaskSearch.vue";
 import AppTaskAdd from "./components/AppTaskAdd.vue";
-import { computed, ref } from "@vue/composition-api";
+import { computed, ref, watch } from "@vue/composition-api";
 
 export default {
   name: "app",
   setup() {
     // Modelo Local
+    // TODO: Documentación: https://v3.vuejs.org/guide/composition-api-introduction.html#reactive-variables-with-ref
     const tasks = ref(tasksList);
     const search = ref("");
     // Propiedades computadas
     const filteredTasks = computed(() => {
       return tasks.value.filter((task) => task.title.includes(search.value));
     });
+    // Watchers
+    // TODO: Documentación: https://v3.vuejs.org/guide/composition-api-introduction.html#reacting-to-changes-with-watch
+    // Getter function to watch
+    watch(() => {
+      console.log(tasks.value.length);
+    });
+    // Reactive reference and callback (newValue, oldValue)
+    watch(search, (newValue, oldValue) => {
+      console.log(`Old:${oldValue} | | New:${newValue}`);
+    });
+
     // Metodos locales
     function addTask(newTask) {
       tasks.value.push({
